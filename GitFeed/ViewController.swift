c5d50c988c143a53cfe15cfb1790842e16620816
xanderchchiang <XanderCHChiang@mobiledrivetech.com>
2023-03-17 00:59:29 +0800

    Initial Commit

---
+++ GitFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitFeed
+//
+//  Created by xander on 2023/3/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
